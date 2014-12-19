8f503173fa09152cddeb0870e253e93390c81680
knonn <romain.besland@gmail.com>
2014-12-19 17:09:30 +0100

    control update

--- lcd/lcd_control.py
+++ lcd/lcd_control.py
@@ -2,21 +2,43 @@
 # Auteur : knonn
 # Utilitaire pour l'allumage ou l'extinction d'un LCD 16x2 branche au Raspberry Pi via un module relai
 import RPi.GPIO as GPIO
+import subprocess
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(26, GPIO.OUT)
+
 
 while 1:
 	a = raw_input("What do you want to do ?\n \t1. Switch ON the LCD\n \t2. Switch OFF the LCD\n\nChoice number : ")
 	a = int(a)
 
 	if a == 1:
+		GPIO.output(12, True)
+		GPIO.output(16, True)
+		GPIO.output(18, True)
+		GPIO.output(22, True)
+		GPIO.output(24, True)
+		GPIO.output(26, True)
 		GPIO.output(7, False)
+		subprocess.Popen(["sudo", "/etc/init.d/lcd"])
 		print("Setting LCD ON : \033[92mDone.")
 		break
 	elif a == 2:
+		GPIO.output(12, False)
+                GPIO.output(16, False)
+                GPIO.output(18, False)
+                GPIO.output(22, False)
+                GPIO.output(24, False)
+                GPIO.output(26, False)		
 		GPIO.output(7, True)
+		subprocess.call(["sudo", "killall", "lcd"])
 		print("Setting LCD OFF : \033[92mDone.")
 		break
 	else:
