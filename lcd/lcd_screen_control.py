52c1aa7a3295fae5be57270d03c2aa8fca8cf211
Besland Romain <romain.besland@gmail.com>
2015-03-10 12:11:46 +0100

    add GNU GPL licence

--- lcd/lcd_screen_control.py
+++ lcd/lcd_screen_control.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python
-# Auteur : knonn
-# Utilitaire pour l'allumage ou l'extinction d'un LCD 16x2 branche au Raspberry Pi via un module relai
+
+#    Utilitaire pour l'allumage ou l'extinction d'un LCD 16x2 branche au Raspberry Pi via un module relai
+#    Copyright (C) 2015  <romain.besland@cryptolab.net>
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import RPi.GPIO as GPIO
 import subprocess
 
